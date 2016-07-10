ac192d1cb8b39d945228e97e864994251d02f7f0
Kelly Markiewicz <kellypoon@gmail.com>
2016-07-10 09:27:08 -0600

    Added a method to module Listable so three classes can use it

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,11 @@
 module Listable
   # Listable methods go here
+  # methods related to formatting listable data
+  # include Listable at top of page
+
+  def format_description(description)
+  	"#{description}".ljust(30)
+  end	
+
+
 end
